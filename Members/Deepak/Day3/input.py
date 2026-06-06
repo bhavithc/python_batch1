a7d2cd938c54d1a55a1102b28f3931bf84dcca37
deepak a <deepakdeepu464@gmail.com>
2026-06-06 14:00:15 +0530

    Fifth commit for input parameter
    Signed-off-by: deepak a <deepakdeepu464@gmail.com>

---
+++ Members/Deepak/Day3/input.py
@@ -0,0 +1,23 @@
+# print("Please enter first number")
+# a = input() # 15
+# x = int(a) 
+
+# print("please enter second number")
+# b = input() #20
+# y = int(b)
+
+
+# print("sum of {}  and {} is {}".format(x, y, x+y))
+
+
+# a = input("please enter the first number: \n")  #25
+# b = input("please enter the second number: \n") #20
+
+# print("sum of {} and {} is {}".format(a, b, a+b)) # This output will give merging of two numbers
+
+
+a = int(input("please enter the first number: \n"))  #25
+b = int(input("please enter the second number: \n")) #20
+
+print("sum of {} and {} is {}".format(a, b, a+b))
+
