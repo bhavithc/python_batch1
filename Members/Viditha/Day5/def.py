8076e23d14bc57dd01ef5b9dc16f339c11a12070
unknown <vidi1996.vp@gmail.com>
2026-06-19 23:41:49 +0530

    day5&day6 -assignment and concept practice

---
+++ Members/Viditha/Day5/def.py
@@ -0,0 +1,19 @@
+a=10
+def add(a,b):
+    return a+b
+add(a,20)
+
+
+
+
+def add(a,b):
+    a=10
+    c=a+b
+    print(c) #c is within the add()scoop
+add(a,60)  
+#print(c) // out of scoop 
+ 
+
+
+      
+
