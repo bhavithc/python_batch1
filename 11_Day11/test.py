c4723a171b20dcb1da75c2752e6f02f033c10117
Bhavith C <33711684+bhavithc@users.noreply.github.com>
2026-07-05 09:17:48 +0530

    Day11 (#14)
    * basic of python
    
    * reorg
    
    Signed-off-by: Bhavith C <bhavithc.acharya@gmail.com>
    
    ---------
    
    Signed-off-by: Bhavith C <bhavithc.acharya@gmail.com>

---
+++ 11_Day11/test.py
@@ -0,0 +1,30 @@
+# a = (10, 20)
+# b = (10, 20)
+
+# for item in a:
+#     print(item)
+
+# c = tuple()
+# c.append(10)
+
+
+
+def employee():
+    return ["Dilip", 60, 29]
+
+a = employee()
+
+
+
+# a[0] = "Bhavith"
+
+# name = a[0]
+# salary = a[1]
+# age = a[2]
+name, b, b = a
+
+
+print(f"{name}, {b}, {b}")
+# print(type(employee()))
+# print(a)
+
